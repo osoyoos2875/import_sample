e27966c222a6cd953f40667c44f3f4231184a01a
Yoichiro Miki <y.miki.485@gmail.com>
2023-12-02 16:56:12 +0900

    changed admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from import_export.resources import ModelResource
+from import_export.admin import ImportMixin
+from import_export.formats import base_formats
+from .models import Book
 
-# Register your models here.
+class BookResource(ModelResource):
+    class Meta:
+        model = Book
+
+        import_order = ('id', 'name', 'status')
+        import_id_fields = ['id']
+
+class BookAdmin(ImportMixin, admin.ModelAdmin):
+    list_display = ('id', 'name', 'status')
+    resource_class = BookResource
+    formats = [base_formats.CSV]
+
+admin.site.register(Book, BookAdmin)
